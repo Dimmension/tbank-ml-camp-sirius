64d043a8428c215988297dbe3e2a43e3bf0fb164
KornilovaK <Katya.Kornil10@gmail.com>
2024-12-13 08:26:49 +0000

    add dinamic result saveing

--- src/ensemple/app.py
+++ src/ensemple/app.py
@@ -1,4 +1,5 @@
 import json
+import pandas as pd
 from llm_utils import LLMHandler
 from rag_utils import RAGHadler
 from retriever_utils import RetrievalSystem
@@ -14,17 +15,17 @@ llm_model = LLMHandler(
     0.97
 )
 rag = RAGHadler(retriever, llm_model, 3)
+l = 100
 
 if __name__ == '__main__':
-    with open('././data/data_testing_full.json', 'r') as f:
-        data = json.load(f)
-    
+    df = pd.read_csv('././data/llm_val_dataset.csv')
+    output_path = '././data/result.csv'
     result = []
-    true = 0
-    for label in list(data):
-        for query in data[label]:
-            top_labels, predicted_label = rag.predict_label(query, label)
-            result.append({'query': query, 'initial label': label, 'predicted label': predicted_label})
-            
-    with open('/home/student/katya/tbank-ml-camp-sirius/data/result.json', "w") as f:
-        json.dump(result, f, indent=4)
+    
+    for i, row in df.iterrows():
+        top_labels, predicted_label = rag.predict_label(row['text'], row['intent'])
+        result.append({'query': row['text'], 'initial label': row['true_intent'], 'predicted label': predicted_label})
+        
+        if i % l == 0:
+            with open(output_path, "w") as f:
+                json.dump(result, f, indent=4)
